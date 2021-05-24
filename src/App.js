bacf85e611d47705cd72b84cb32540be493aeeb9
Kyle Moxham <kyle.moxham@built.co.uk>
2021-05-24 08:27:51 +0100

    Remove unnecessary files and add Loans as main content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Loans from './components/Loans';
+import data from './data/current-loans.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Loans data={data.loans} />
   );
 }
 
